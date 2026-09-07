ac187ff19c87bbf87fe428512e7110a83d2d5c1f
davi arruda <daviarruda092@gmail.com>
2026-09-07 11:52:41 -0300

    + departamentDAOJDBC e imp extra

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -2,9 +2,13 @@ package model.dao;
 
 import db.DB;
 import model.dao.impl.SellerDAOJDBC;
+import model.dao.impl.DepartmentDAOJDBC;
 
 public class DaoFactory {
 	public static SellerDAO craeteSellerDao() {
 		return new SellerDAOJDBC(DB.getConnection());//para n expor a implemen
+	}
+	public static DepartmentDAO createDepartmentDao() {
+		return new DepartmentDAOJDBC(DB.getConnection());
 	}
 }
